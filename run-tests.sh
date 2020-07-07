61867385b40379d1266cbd4827f0871328b22e96
Pablo Panero <pablo.panero@cern.ch>
2020-07-07 13:54:46 +0200

    tests: fix isort flags for version 5

--- run-tests.sh
+++ run-tests.sh
@@ -7,7 +7,7 @@
 # it under the terms of the MIT License; see LICENSE file for more details.
 
 pydocstyle invenio_resources tests docs && \
-isort -rc -c -df && \
+isort invenio_resources tests --check-only --diff && \
 check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test
