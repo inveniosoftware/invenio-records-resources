3d1190b18d7d9a9a93eac906462df1368859aeee
Guillaume Viger <fenekku@fenekku.com>
2020-07-22 11:11:53 -0500

    release: v0.2.0
    Need to release to get the flask-resources dependency

--- invenio_records_resources/version.py
+++ invenio_records_resources/version.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
 #
 # Invenio-Records-Resources is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -12,4 +13,4 @@ This file is imported by ``invenio_records_resources.__init__``,
 and parsed by ``setup.py``.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
