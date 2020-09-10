a7c68aaf82b1a4f681982ed8843bac49dd7fea20
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-09-10 16:33:03 +0200

    tests: move module level test to root

--- tests/test_invenio_resources.py
+++ tests/test_invenio_resources.py
@@ -16,7 +16,6 @@ from invenio_records_resources import InvenioRecordsResources
 def test_version():
     """Test version import."""
     from invenio_records_resources import __version__
-
     assert __version__
 
 
