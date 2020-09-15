f24375d1cadaa86801ffb034a09f9ac67394a2da
Pablo Panero <pablo.panero@cern.ch>
2020-09-15 11:30:45 +0200

    tests: use new docker-services-cli

--- run-tests.sh
+++ run-tests.sh
@@ -6,6 +6,12 @@
 # Invenio-Records-Resources is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
 
+python -m pydocstyle invenio_records_resources tests docs && \
+python -m isort invenio_records_resources tests --check-only --diff && \
 python -m check_manifest --ignore ".travis-*" && \
 python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
+docker-services-cli up es postgresql redis
 python -m pytest
+tests_exit_code=$?
+docker-services-cli down
+exit "$tests_exit_code"
