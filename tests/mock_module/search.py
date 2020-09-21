30b679cb5212b5026d64da614cd3913c2a99e248
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2020-09-21 14:11:38 +0200

    facets: add tests for aggs and post_filtering

--- tests/mock_module/search.py
+++ tests/mock_module/search.py
@@ -1,6 +1,18 @@
 """Example search."""
 
+from elasticsearch_dsl import Q
+
 from invenio_search import RecordsSearch as RecordsSearchBase
+
+
+def terms_filter(field):
+    """Create a term filter.
+    :param field: Field name.
+    :returns: Function that returns the Terms query.
+    """
+    def inner(values):
+        return Q('terms', **{field: values})
+    return inner
 
 
 class RecordsSearch(RecordsSearchBase):
--- tests/mock_module/service.py
+++ tests/mock_module/service.py
@@ -3,12 +3,13 @@
 from invenio_records_resources.records.resolver import UUIDResolver
 from invenio_records_resources.services import RecordService, \
     RecordServiceConfig
-from invenio_records_resources.services.records.params import SortParam
+from invenio_records_resources.services.records.params import FacetsParam, \
+    SortParam
 
 from .api import Record
 from .permissions import PermissionPolicy
 from .schema import RecordSchema
-from .search import RecordsSearch
+from .search import RecordsSearch, terms_filter
 
 
 class ServiceConfig(RecordServiceConfig):
@@ -18,7 +19,38 @@ class ServiceConfig(RecordServiceConfig):
     record_cls = Record
     schema = RecordSchema
     search_cls = RecordsSearch
-
+    search_sort_default = 'bestmatch'
+    search_sort_default_no_query = 'mostrecent'
+    search_sort_options = dict(
+        bestmatch=dict(
+            title='Best match',
+            fields=['_score'],
+        ),
+        mostrecent=dict(
+            title='Most recent',
+            fields=['-created'],
+        ),
+    )
+    search_facets_options = {
+        'aggs': {
+            'type': {
+                'terms': {'field': 'metadata.type.type'},
+                'aggs': {
+                    'subtype': {
+                        'terms': {'field': 'metadata.type.subtype'},
+                    }
+                }
+            }
+        },
+        'post_filters': {
+            'subtype': terms_filter('metadata.type.subtype'),
+            'type': terms_filter('metadata.type.type'),
+        }
+    }
+    search_params_interpreters_cls = [
+        SortParam,
+        FacetsParam
+    ]
 
 class Service(RecordService):
     """Mock service."""
