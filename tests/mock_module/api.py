7eb371a640658e25f4e425abb720bf0f6185a98d
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-09-28 14:13:03 +0200

    global: add license headers

--- tests/mock_module/api.py
+++ tests/mock_module/api.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
 """Example of a record API."""
 
 from datetime import datetime
