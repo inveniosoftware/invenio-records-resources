bd823651cfdb90c8d811b05f8484ca03316a8501
Pablo Panero <pablo.panero@cern.ch>
2020-11-17 17:37:11 +0100

    service: add file support

--- invenio_records_resources/services/files/service.py
+++ invenio_records_resources/services/files/service.py
@@ -10,45 +10,61 @@
 """Record Service API."""
 
 from ..base import Service
-from .config import FileMetadataServiceConfig, FileServiceConfig
+from ..records import RecordService
+from .config import RecordFileServiceConfig
 
 
-class FileService(Service):
-    """File Service.
+class FileServiceMixin(Service):
+    """File service mixin.
 
-    Because actual files are not Records, it makes sense for them to have their
-    own service.
+    This service is meant to work as a mixin, along a service that
+    supports records. It is not meant to work as an standalone service.
     """
 
-    default_config = FileServiceConfig
+    default_config = None  # It is defined when the mixin is used.
 
     #
     # High-level API
     #
-    def read(self, id_, identity):
-        """Retrieve ."""
-        # TODO: IMPLEMENT ME!
-        return self.result_item()
+
+    def list_files(self, id_, identity):
+        """List the files of a record."""
+        return {}
+
+    def init_file_upload(self, id_, identity):
+        """Initialize the file upload for the record."""
+        return {}
+
+    def delete_all_files(self, id_, identity):
+        """Delete all the files of the record.."""
+        return {}
+
+    def update_file_metadata(self, id_, file_id, identity, data):
+        """Update the metadata of a file."""
+        return {}
+
+    def read_file_metadata(self, id_, file_id, identity):
+        """Read the metadata of a file."""
+        return {}
+
+    def commit_file(self, id_, file_id, identity, links_config):
+        """Commit a file upload."""
+        return {}
+
+    def delete_file(self, id_, file_id, identity, revision_id):
+        """Delete a single file."""
+        return {}
+
+    def save_file(self, id_, file_id, identity, content, links_config):
+        """Save file content."""
+        return {}
+
+    def retrieve_file(self, id_, file_id, identity, links_config):
+        """Retrieve file content."""
+        return {}
 
 
-class FileMetadataService(Service):
-    """File Metadata Service.
+class RecordFileService(RecordService, FileServiceMixin):
+    """Record service with files support."""
 
-    Because file metadata isn't shaped like a Record, it makes sense for them
-    to have their own service.
-    """
-
-    default_config = FileMetadataServiceConfig
-
-    #
-    # High-level API
-    #
-    def read(self, id_, identity):
-        """Retrieve ."""
-        # TODO: IMPLEMENT ME!
-        return self.result_item()
-
-    def search(self, querystring, identity, pagination=None, *args, **kwargs):
-        """Search for records matching the querystring."""
-        # TODO: IMPLEMENT ME!
-        return self.result_list()
+    default_config = RecordFileServiceConfig
