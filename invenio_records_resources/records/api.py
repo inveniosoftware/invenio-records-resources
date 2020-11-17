4a326946f7e011d26a2d31ffadcc2d72c4e715da
Alexander Ioannidis <a.ioannidis@cern.ch>
2020-11-18 00:48:16 +0100

    records: files system field
    * Closes #144.

--- invenio_records_resources/records/api.py
+++ invenio_records_resources/records/api.py
@@ -9,9 +9,12 @@
 
 """Records API."""
 
+from invenio_db import db
+from invenio_files_rest.models import FileInstance, ObjectVersion
 from invenio_records.api import Record as RecordBase
 from invenio_records.dumpers import ElasticsearchDumper
 from invenio_records.systemfields import DictField, SystemFieldsMixin
+from invenio_records.systemfields.model import ModelField
 
 
 class Record(RecordBase, SystemFieldsMixin):
@@ -38,3 +41,113 @@ class Record(RecordBase, SystemFieldsMixin):
 
     #: Concrete implementations need to implement the index field.
     # index = IndexField(...)
+
+    #: Concrete implementations need to implement the files field.
+    # files = FilesField(...)
+
+
+class RecordFile(RecordBase, SystemFieldsMixin):
+    """Base class for record file APIs."""
+
+    @classmethod
+    def get_by_key(cls, record_id, key):
+        """Get a record file by record ID and filename/key."""
+        with db.session.no_autoflush:
+            obj = cls.model_cls.query.filter(
+                cls.record_id == record_id, cls.key == key).one_or_none()
+            if obj:
+                return cls(obj.data, model=obj)
+
+    @classmethod
+    def list_by_record(cls, record_id):
+        """List all record files by record ID."""
+        for obj in cls.model_cls.query.filter(cls.record_id == record_id):
+            yield cls(obj.data, model=obj)
+
+    @property
+    def file(self):
+        """File wrapper object."""
+        if self.object_version:
+            return File(object_model=self.object_version)
+
+    send_signals = False
+    enable_jsonref = False
+
+    #: Default model class used by the record API (specify in subclass).
+    model_cls = None
+
+    #: Default dumper (which happens to also be used for indexing).
+    dumper = ElasticsearchDumper()
+
+    #: Metadata system field.
+    metadata = DictField(clear_none=True, create_if_missing=True)
+
+    key = ModelField()
+    object_version_id = ModelField()
+    object_version = ModelField(dump=False)
+    record_id = ModelField()
+    record = ModelField(dump=False)
+
+    def __repr__(self, ):
+        """Represenation string for the record file."""
+        return f"<{type(self).__name__}({self.key}, {self.metadata})"
+
+
+class File:
+    """File wrapper object."""
+
+    def __init__(self, object_model=None, file_model=None):
+        """Initialize the file wrapper object."""
+        self.object_model = object_model
+        self.file_model = file_model or object_model.file
+
+    @classmethod
+    def from_dict(cls, data, bucket):
+        """Construct a file wrapper from a dictionary."""
+        file_args = dict(
+            id=data['file_id'],
+            storage_class=data.get('storage_class'),
+            size=data.get('size'),
+            checksum=data.get('checksum'),
+        )
+        if 'uri' in data:
+            file_args['uri'] = data['uri']
+
+        fi = FileInstance(**file_args)
+        obj = ObjectVersion(
+            version_id=data['version_id'],
+            key=data['key'],
+            file_id=data['file_id'],
+            _mimetype=data['mimetype'],
+            is_head=True,
+            bucket=bucket,
+            bucket_id=data.get('bucket_id', bucket.id),
+        )
+        return cls(object_model=obj, file_model=fi)
+
+    def dumps(self):
+        """Dump file model attributes of the object."""
+        return {
+            'version_id': str(self.object_model.version_id),
+            'key': self.object_model.key,
+            'bucket_id': str(self.object_model.bucket_id),
+            'file_id': str(self.object_model.file_id),
+            'uri': str(self.object_model.file.uri),
+            'storage_class': self.object_model.file.storage_class,
+            'mimetype': self.object_model.mimetype,
+            'size': self.object_model.file.size,
+            'checksum': self.object_model.file.checksum,
+        }
+
+    def __getattr__(self, name):
+        """Override to get attributes from ObjectVersion and FileInstance."""
+        ret = getattr(self.object_model, name, None)
+        if not ret:
+            ret = getattr(self.file_model, name, None)
+        if not ret:
+            raise AttributeError
+        return ret
+
+    def __repr__(self):
+        """Represntation string for the file wrapper object."""
+        return f"<{type(self).__name__}({self.key}, {self.file_id})"
---
+++ tests/records/test_systemfield_files.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+# Copyright (C) 2020 Northwestern University.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Files field tests."""
+
+
+from io import BytesIO
+
+import pytest
+from invenio_files_rest.models import Bucket, FileInstance, Location, \
+    ObjectVersion
+from invenio_records.systemfields import ModelField
+from mock_module import models
+from mock_module.api import Record as RecordBase
+from mock_module.api import RecordFile
+
+from invenio_records_resources.records.systemfields.files import FilesField
+
+
+# Define a files-enabled record class
+class Record(RecordBase):
+
+    files = FilesField(store=True, file_cls=RecordFile)
+    bucket_id = ModelField()
+    bucket = ModelField(dump=False)
+
+
+@pytest.fixture()
+def location(db, tmp_path):
+    """File system location."""
+    loc = Location(
+        name='testloc',
+        uri=str(tmp_path),
+        default=True
+    )
+    db.session.add(loc)
+    db.session.commit()
+    return loc
+
+
+def test_class_attribute_access():
+    """Test that field is returned."""
+    assert isinstance(Record.files, FilesField)
+
+
+def test_record_files_creation(base_app, db, location):
+    """Test record files bucket creation."""
+    record = Record.create({})
+    assert record.bucket_id
+    assert record.bucket
+    assert record.files.enabled is True
+    assert record.files.default_preview is None
+    assert record.files.order == []
+    assert len(record.files) == 0
+    assert set(record.files) == set()
+
+    assert record['files']
+    assert record['files']['enabled'] is True
+    assert record['files']['default_preview'] is None
+    assert record['files']['order'] == []
+    assert record['files']['entries'] == {}
+
+
+def test_record_files_deletion(base_app, db, location):
+    """Test record files bucket deletion."""
+    record = Record.create({})
+    bucket_id = record.bucket_id
+    db.session.commit()
+    assert Bucket.query.count() == 1
+    assert Bucket.query.get(bucket_id)
+
+    record.delete()
+    db.session.commit()
+    assert Bucket.query.count() == 0
+
+
+def test_record_files_operations(base_app, db, location):
+    """Test record files bucket creation."""
+    record = Record.create({})
+
+    # Initialize a file
+    record.files['test.pdf'] = {'description': 'A test file.'}
+    rf = record.files['test.pdf']
+    assert rf.key == 'test.pdf'
+    assert rf.object_version is None
+    assert rf.object_version_id is None
+    assert rf.record_id is not None
+    assert rf.record_id == record.id
+    assert rf.metadata == {'description': 'A test file.'}
+    assert rf['metadata'] == {'description': 'A test file.'}
+    db.session.commit()
+
+    assert models.RecordFile.query.count() == 1
+    assert ObjectVersion.query.count() == 0
+
+    # Update the file's metadata
+    record.files['test.pdf'] = {'description': 'A new description'}
+    rf = record.files['test.pdf']
+    assert rf.key == 'test.pdf'
+    assert rf.object_version is None
+    assert rf.object_version_id is None
+    assert rf.record_id is not None
+    assert rf.record_id == record.id
+    assert rf.metadata == {'description': 'A new description'}
+    assert rf['metadata'] == {'description': 'A new description'}
+
+    # Add an actual file
+    dummy_file = BytesIO(b'testfile')
+    record.files['test.pdf'] = dummy_file
+    rf = record.files['test.pdf']
+    assert rf.key == 'test.pdf'
+    assert rf.object_version
+    assert rf.object_version_id
+    assert rf.object_version.key == rf.key == 'test.pdf'
+    assert rf.object_version.file
+    assert rf.metadata == {'description': 'A new description'}
+    assert rf['metadata'] == {'description': 'A new description'}
+    db.session.commit()
+
+    assert FileInstance.query.count() == 1
+    assert ObjectVersion.query.count() == 1
+
+
+def test_record_files_store(base_app, db, location):
+    """Test JSON stored for files."""
+    record = Record.create({})
+
+    # Add a file with bytes + metadata
+    record.files['f1.pdf'] = (
+        BytesIO(b'testfile'),
+        {'description': 'Test file'}
+    )
+    # Add a file with only bytes
+    record.files['f2.pdf'] = BytesIO(b'testfile')
+    # Add a file with only metadata
+    record.files['f3.pdf'] = {'description': 'Metadata only'}
+
+    rf1 = record.files['f1.pdf']
+    rf2 = record.files['f2.pdf']
+    rf3 = record.files['f3.pdf']
+    record.commit()
+    assert record['files']['meta'] == {
+        'f1.pdf': {'description': 'Test file'},
+        'f2.pdf': None,
+        'f3.pdf': {'description': 'Metadata only'},
+    }
+    assert record['files']['entries'] == {
+        rf.key: {
+            'bucket_id': str(record.bucket_id),
+            'checksum': rf.file.checksum,
+            'file_id': str(rf.file.file_id),
+            'key': rf.key,
+            'mimetype': rf.file.mimetype,
+            'size': rf.file.size,
+            'storage_class': rf.file.storage_class,
+            'uri': rf.file.uri,
+            'version_id': str(rf.object_version_id),
+        } for rf in (rf1, rf2)
+    }
