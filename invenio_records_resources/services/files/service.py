90c931a152874954e87fec26cea4ddec731645cd
Georgios Lignos <glignos93@gmail.com>
2020-11-23 14:19:41 +0100

    services-files: implement functionality

--- invenio_records_resources/services/files/service.py
+++ invenio_records_resources/services/files/service.py
@@ -9,7 +9,12 @@
 
 """Record Service API."""
 
-from ..base import Service
+from copy import deepcopy
+
+from invenio_db import db
+from invenio_files_rest.errors import FileSizeError
+from invenio_files_rest.models import ObjectVersion
+
 from ..records import RecordService
 from ..records.schema import MarshmallowServiceSchema
 from .config import RecordFileServiceConfig
@@ -36,7 +41,8 @@ dummy_file_entry = {
 
 # FIXME: Two functions and one property have to be prefixed `file_` to avoid
 #        collisions when using the mixing e.g. along with a RecordService.
-class FileServiceMixin(Service):
+
+class FileServiceMixin:
     """File service mixin.
 
     This service is meant to work as a mixin, along a service that
@@ -73,41 +79,167 @@ class FileServiceMixin(Service):
     # High-level API
     #
 
-    def list_files(self, id_, identity):
+    def list_files(self, id_, identity, links_config=None):
         """List the files of a record."""
-        return self.file_result_list()
+        record = self.record_cls.pid.resolve(id_)
+        self.require_permission(identity, "read", record=record)
+        return self.file_result_list(
+            self,
+            identity,
+            results=record.files.values(),
+            links_config=links_config,
+        )
 
-    def init_file(self, id_, identity, data):
+    def init_files(self, id_, identity, data, links_config=None):
         """Initialize the file upload for the record."""
-        return self.file_result_item()
+        record = self.record_cls.pid.resolve(id_)
+        # TODO implement permission, limit files?
+        self.require_permission(identity, "create", record=record)
+        # TODO: Load via marshmallow schema?
+        results = []
+        for file_metadata in data:
+            temporary_obj = deepcopy(file_metadata)
+            results.append(
+                record.files.create(
+                    temporary_obj.pop('key'), data=temporary_obj))
+        # TODO: maybe do this automatically in the files field
+        record.commit()
+        db.session.commit()
+        return self.file_result_list(
+            self,
+            identity,
+            results,
+            links_config,
+        )
 
-    def delete_all_files(self, id_, identity):
-        """Delete all the files of the record.."""
-        return True
-
-    def update_file_metadata(self, id_, file_id, identity, data):
+    def update_file_metadata(
+            self, id_, file_key, identity, data, links_config=None):
         """Update the metadata of a file."""
-        return self.file_result_item()
+        record = self.record_cls.pid.resolve(id_)
+        self.require_permission(identity, "create", record=record)
+        rf = record.files.update(file_key, data=data)
+        record.commit()
+        db.session.commit()
+        return self.file_result_item(
+            self,
+            identity,
+            rf,
+            links_config,
+        )
 
-    def read_file_metadata(self, id_, file_id, identity):
+    def read_file_metadata(self, id_, file_key, identity, links_config=None):
         """Read the metadata of a file."""
-        return self.file_result_item()
+        record = self.record_cls.pid.resolve(id_)
+        return self.file_result_item(
+            self,
+            identity,
+            record.files[file_key],
+            links_config,
+        )
 
-    def commit_file(self, id_, file_id, identity, links_config):
+    # TODO: `commit_file` might vary based on your storage backend (e.g. S3)
+    def commit_file(self, id_, file_key, identity, links_config=None):
         """Commit a file upload."""
-        return self.file_result_item()
+        record = self.record_cls.pid.resolve(id_)
+        file_obj = ObjectVersion.get(record.bucket.id, file_key)
+        if not file_obj:
+            raise Exception(f'File with key {file_key} not uploaded yet.')
+        # TODO: Add other checks here (e.g. verify checksum, S3 upload)
+        record.files[file_key] = file_obj
+        record.commit()
+        db.session.commit()
+        return self.file_result_item(
+            self,
+            identity,
+            record.files[file_key],
+            links_config,
+        )
 
-    def delete_file(self, id_, file_id, identity, revision_id):
+    def delete_file(self, id_, file_key, identity, links_config=None):
         """Delete a single file."""
-        return True
+        record = self.record_cls.pid.resolve(id_)
+        deleted_file = record.files.delete(file_key)
+        record.commit()
+        db.session.commit()
+        return self.file_result_item(
+            self,
+            identity,
+            deleted_file,
+            links_config,
+        )
 
-    def save_file(self, id_, file_id, identity, content, links_config):
+    def delete_all_files(self, id_, identity, links_config=None):
+        """Delete all the files of the record."""
+        record = self.record_cls.pid.resolve(id_)
+        self.require_permission(identity, "delete", record=record)
+        results = []
+        for file in record.files:
+            results.append(record.files.delete(file.key))
+        return self.file_result_list(
+            self,
+            identity,
+            results,
+            links_config,
+        )
+
+    # POST /api/records/1234/files
+    # [ {'key': 'article.pdf', 'checksum': '...'}, ]
+    #
+    # [{ 'links': {'upload': '/api/records/1234/files/article.pdf' } }]
+    # PUT /api/records/1234/files/article.pdf --upload-file bob.png
+    # POST /api/records/1234/files/article.pdf/commit
+    # "Wrong file bruder"
+
+    # PUT /api/records/1234/files/article.pdf --upload-file bob-correct.png
+    #
+    #
+
+    def set_file_content(
+            self, id_, file_key, identity, stream,
+            content_length=None, links_config=None):
         """Save file content."""
-        return self.file_result_item()
+        # TODO stream not exhausted
+        record = self.record_cls.pid.resolve(id_)
+        rf = record.files.get(file_key)
+        if not rf or rf.file:
+            # TODO bad request
+            return {}
 
-    def retrieve_file(self, id_, file_id, identity, links_config):
+        bucket = record.bucket
+        size_limit = bucket.size_limit
+        if content_length and size_limit and content_length > size_limit:
+            desc = 'File size limit exceeded.' \
+                if isinstance(size_limit, int) else size_limit.reason
+            raise FileSizeError(description=desc)
+        # DB connection?
+        # re uploading failed upload?
+
+        with db.session.begin_nested():
+            # TODO: in case we want to update a file, this keeps the old
+            # FileInstance. It might be better to call ObjectVersion.remove()
+            # before or after the "set_content"
+            obj = ObjectVersion.create(bucket, file_key)
+            obj.set_contents(
+                stream, size=content_length, size_limit=size_limit)
+        db.session.commit()
+        return self.file_result_item(
+            self,
+            identity,
+            record.files[file_key],
+            links_config,
+        )
+
+    def get_file_content(self, id_, file_key, identity, links_config=None):
         """Retrieve file content."""
-        return {}
+        record = self.record_cls.pid.resolve(id_)
+        # TODO Signal here or in resource?
+        # file_downloaded.send(file_obj)
+        return self.file_result_item(
+            self,
+            identity,
+            record.files[file_key],
+            links_config,
+        )
 
 
 class RecordFileService(RecordService, FileServiceMixin):
