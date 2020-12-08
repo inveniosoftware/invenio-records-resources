b0c50b0c60741400163227c6eb4d737e45a071e3
Guillaume Viger <fenekku@fenekku.com>
2020-12-08 11:46:53 -0600

    ci: update to docker-services-cli 0.3.0 [+]
    - :warning: that dependency is managed by pytest-invenio
      (and new pytest-invenio has not been released yet)
      so developers need to explicitly pip install --upgrade
      docker-services-cli locally.

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2020 CERN.
@@ -12,10 +12,16 @@ set -o errexit
 # Quit on unbound symbols
 set -o nounset
 
+# Always bring down docker services
+function cleanup() {
+    eval "$(docker-services-cli down --env)"
+}
+
+trap cleanup EXIT
+
 python -m check_manifest --ignore ".*-requirements.txt"
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
-docker-services-cli up es postgresql redis
+eval "$(docker-services-cli --verbose up --search ${ES_VERSION:-elasticsearch} --db ${POSTGRESQL_VERSION:-postgresql} --cache redis --env)"
 python -m pytest
 tests_exit_code=$?
-docker-services-cli down
 exit "$tests_exit_code"
