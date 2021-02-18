b53f80e26474cf0f8f5a86dfcfb3357bd87ec450
Guillaume Viger <fenekku@fenekku.com>
2021-02-18 15:53:34 -0600

    release: v0.12.2

--- invenio_records_resources/version.py
+++ invenio_records_resources/version.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020 CERN.
-# Copyright (C) 2020 Northwestern University.
+# Copyright (C) 2020-2021 CERN.
+# Copyright (C) 2020-2021 Northwestern University.
 #
 # Invenio-Records-Resources is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -13,4 +13,4 @@ This file is imported by ``invenio_records_resources.__init__``,
 and parsed by ``setup.py``.
 """
 
-__version__ = "0.12.1"
+__version__ = "0.12.2"
