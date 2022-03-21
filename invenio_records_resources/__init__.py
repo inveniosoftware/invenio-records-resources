73c89486d2920d2f5663897dbcc6af20e9eaec1f
Alexander Ioannidis <a.ioannidis@cern.ch>
2022-03-21 17:27:00 +0100

    release: v0.19.3

--- invenio_records_resources/__init__.py
+++ invenio_records_resources/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020 CERN.
+# Copyright (C) 2020-2022 CERN.
 #
 # Invenio-Records-Resources is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -10,7 +10,7 @@
 
 from .ext import InvenioRecordsResources
 
-__version__ = "0.19.2"
+__version__ = "0.19.3"
 
 
 __all__ = ("__version__", "InvenioRecordsResources")
