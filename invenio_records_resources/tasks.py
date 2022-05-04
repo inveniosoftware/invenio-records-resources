49f9dbda3d68cb65c384de8032b5967a808fc80d
Pablo Panero <pablo.panero@cern.ch>
2022-05-04 17:25:08 +0200

    tasks: peek message queue and spawn indexers

--- invenio_records_resources/tasks.py
+++ invenio_records_resources/tasks.py
@@ -9,8 +9,12 @@
 """Celery tasks for async processing."""
 
 import arrow
+from celery import current_app as current_celery_app
 from celery import shared_task
+from flask import current_app
 from invenio_access.permissions import system_identity
+from invenio_indexer.proxies import current_indexer_registry
+from invenio_indexer.tasks import process_bulk_queue
 
 from .proxies import current_notifications_registry, current_service_registry
 
@@ -30,3 +34,18 @@ def send_change_notifications(record_type, records_info):
     handlers = current_notifications_registry.get(record_type)
     for notif_handler in handlers:
         notif_handler(system_identity, record_type, records_info, task_start)
+
+
+@shared_task(ignore_result=True)
+def manage_indexer_queues():
+    """Peeks into queues and spawns bulk indexers."""
+    channel = current_celery_app.connection().channel()
+    indexers = current_indexer_registry.all()
+
+    for name, indexer in indexers.items():
+        queue = indexer.mq_queue.bind(channel)
+        _, num_messages, num_consumers = queue.queue_declare()
+        max_consumers = current_app.config["INDEXER_MAX_BULK_CONSUMERS"]
+
+        if num_messages > 0 and num_consumers < max_consumers:
+            process_bulk_queue.delay(indexer_name=name)
