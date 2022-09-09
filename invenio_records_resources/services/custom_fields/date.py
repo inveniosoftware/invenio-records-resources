6b4b30015287995ec055060d62e3e9c7b6899f01
Pablo Panero <pablo.panero@cern.ch>
2022-09-09 13:20:50 +0200

    custom fields: add support for dump and load fields

--- invenio_records_resources/services/custom_fields/date.py
+++ invenio_records_resources/services/custom_fields/date.py
@@ -7,8 +7,14 @@
 
 """Custom Fields for InvenioRDM."""
 
+import calendar
+
+from arrow import Arrow
+from edtf import parse_edtf
+from edtf.parser.edtf_exceptions import EDTFParseException
 from marshmallow import fields
 from marshmallow_utils.fields import EDTFDateString, ISODateString
+from pytz import utc
 
 from .base import BaseListCF
 
@@ -36,4 +42,65 @@ class EDTFDateStringCF(BaseListCF):
     @property
     def mapping(self):
         """Return the mapping."""
-        return {"type": "date", "fields": {"date_range": {"type": "date_range"}}}
+        return {
+            "type": "object",
+            "properties": {
+                "date": {"type": "keyword"},
+                "date_range": {"type": "date_range"},
+            },
+        }
+
+    @property
+    def field(self):
+        """Marshmallow field custom fields."""
+        _schema = EDTFDateString(**self._field_args)
+        if self._multiple:
+            return fields.List(_schema)
+        return _schema
+
+    @classmethod
+    def _format_date(cls, date):
+        """Format the given date into ISO format."""
+        arrow = Arrow.fromtimestamp(calendar.timegm(date), tzinfo=utc)
+        return arrow.date().isoformat()
+
+    @classmethod
+    def _calculate_date_range(cls, date):
+        pd = parse_edtf(date)
+        return {
+            "date": date,
+            "date_range": {
+                "gte": cls._format_date(pd.lower_strict()),
+                "lte": cls._format_date(pd.upper_strict()),
+            },
+        }
+
+    def dump(self, data, cf_key="custom_fields"):
+        """Dump the custom field.
+
+        Gets both the record and the custom fields key as parameters.
+        This supports the case where a field is based on others, both
+        custom and non-custom fields.
+        """
+        dates = data[cf_key].get(self.name)
+        if dates:
+            try:
+                if self._multiple:
+                    for date in dates:
+                        data[cf_key][self.name] = self._calculate_date_range(date)
+                else:
+                    # dates is just one date
+                    data[cf_key][self.name] = self._calculate_date_range(dates)
+            except (EDTFParseException):
+                pass
+
+    def load(self, record, cf_key="custom_fields"):
+        """Load the custom field.
+
+        Gets both the record and the custom fields key as parameters.
+        This supports the case where a field is based on others, both
+        custom and non-custom fields.
+        """
+        date = record.get(cf_key, {}).pop(self.name, None)
+        if date:
+            record[cf_key][self.name] = date["date"]
