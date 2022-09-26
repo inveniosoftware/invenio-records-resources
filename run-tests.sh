7c3babe52ac80fa0744e592ed1afba49885f97a7
Mojib <mb_wali@hotmail.com>
2022-09-26 15:00:13 +0200

    ci: adds i18n workflow
    test: adds extract_messages dry command

--- run-tests.sh
+++ run-tests.sh
@@ -31,6 +31,7 @@ function cleanup() {
 trap cleanup EXIT
 
 python -m check_manifest
+python -m setup extract_messages --dry-run
 python -m sphinx.cmd.build -qnNW docs docs/_build/html
 eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-opensearch} --cache ${CACHE:-redis} --env)"
 python -m pytest $@
