340d3a5af2b578ec64cdea15bcda93fcf84c9a18
Nicola Tarocco <ntarocco@gmail.com>
2022-09-29 11:34:00 +0200

    search: remove suggestions support ES<7

--- invenio_records_resources/services/records/queryparser/suggest.py
+++ invenio_records_resources/services/records/queryparser/suggest.py
@@ -8,7 +8,7 @@
 
 """Query parameter interpreter API."""
 
-from invenio_search.engine import dsl, uses_es7
+from invenio_search.engine import dsl
 
 from .query import QueryParser
 
@@ -46,28 +46,13 @@ class SuggestQueryParser(QueryParser):
     For more information see:
     - https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-multi-match-query.html
     - https://www.elastic.co/guide/en/elasticsearch/reference/current/search-as-you-type.html
-
-    Elasticsearch v6 does not support the ``search_as_you_type`` mapping field
-    type. Thus, on Elasticsearch v6 the query is translated to an
-    ``phrase_prefix`` type, which is slower and less user friendly.
     """  # noqa
 
     def __init__(self, identity=None, extra_params=None, **kwargs):
         """Constructor."""
         super().__init__(identity=identity, extra_params=extra_params)
-        self.extra_params.setdefault(
-            "type", "phrase_prefix" if not uses_es7() else "bool_prefix"
-        )
+        self.extra_params.setdefault("type", "bool_prefix")
 
     def parse(self, query_str):
         """Parse the query."""
-        if not uses_es7() and self.extra_params.get("fields"):
-            # On ES v6 we have to filter out field names that uses the
-            # search_as_you_type field names (ending with ._2gram or ._3gram)
-            self.extra_params["fields"] = list(
-                filter(
-                    lambda x: not (x.endswith("._2gram") or x.endswith("._3gram")),
-                    self.extra_params["fields"],
-                )
-            )
         return dsl.Q("multi_match", query=query_str, **self.extra_params)
