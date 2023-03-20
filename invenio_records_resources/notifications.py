cbb42fa7124d285f6c92cf45b0c59bff6ecb5e9c
David Eckhard <dabombeeer@gmail.com>
2023-03-20 16:25:42 +0100

    notifications: add backend payload generator

--- invenio_records_resources/notifications.py
+++ invenio_records_resources/notifications.py
@@ -13,8 +13,7 @@ from abc import ABC, abstractmethod
 from datetime import datetime
 from typing import List
 
-from invenio_i18n import gettext as _
-from invenio_notifications.models import BroadcastNotification
+from invenio_notifications.models import BroadcastNotification, BroadcastRecipient
 
 
 class PayloadGenerator(ABC):
@@ -33,6 +32,28 @@ class RecipientGenerator(ABC):
     def run(self, payload, **kwargs):
         """Generate recipients."""
         return []
+
+
+# NOTE: Move to base backend implementation? It should probably dictate which
+#       information it needs to generate the payload and it knows its id.
+class BackendPayloadGenerator(ABC):
+    """Backend generator for a notification."""
+
+    backend_id: str
+    template: str
+
+    @abstractmethod
+    def run(self, user, **kwargs):
+        """Generate backend payload.
+
+        The returned data should at least consist of:
+            "id": stating the backend id
+            "template": template name
+        """
+        return {
+            "id": self.backend_id,
+            "template": self.template,
+        }
 
 
 class RecipientFilter(ABC):
@@ -56,10 +77,11 @@ class RecipientBackendTransform(ABC):
 class NotificationBuilder:
     """Base notification builder."""
 
-    payload_generator: List[PayloadGenerator]
-    recipients_generator: List[RecipientGenerator]
-    recipients_filters: List[PayloadGenerator]
-    recipient_backend_transform: List[PayloadGenerator]
+    payload_generators: List[PayloadGenerator] = []
+    recipients_generators: List[RecipientGenerator] = []
+    recipients_filters: List[PayloadGenerator] = []
+    backend_payload_generators: List[BackendPayloadGenerator] = []
+    recipient_backend_transforms: List[PayloadGenerator] = []
 
     type = "Notification"
     trigger = {}
@@ -71,18 +93,29 @@ class NotificationBuilder:
     def build(self, **kwargs):
         """Build notification based on specified generators, filters and transformers."""
         data = {}
-        for pg in self.payload_generator:
+        for pg in self.payload_generators:
             data.update(pg.run(**kwargs))
 
+        users = []
+        # NOTE: should only kwargs be passed?
+        for rg in self.recipients_generators:
+            users.extend(rg.run(data, **kwargs))
+
         recipients = []
-        for rg in self.recipients_generator:
-            recipients.extend(rg.run(data, **kwargs))
+        for u in users:
+            backends = [bpg.run(u, **kwargs) for bpg in self.backend_payload_generators]
+            recipients.append(
+                BroadcastRecipient(
+                    user=u,
+                    backends=backends,
+                )
+            )
 
         # NOTE: shall filters only operate on the users and their preferences?
         for rf in self.recipients_filters:
             recipients = rf.run(recipients, **kwargs)
 
-        for rbt in self.recipient_backend_transform:
+        for rbt in self.recipient_backend_transforms:
             recipients = rbt.run(recipients, **kwargs)
 
         # TODO: remove duplicate recipients/users by default or leave it up to the specific Notification?
