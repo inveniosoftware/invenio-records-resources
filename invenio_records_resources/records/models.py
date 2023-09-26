4c4bcc6ec92377b00642a3285f847c8af16b8421
Alex Ioannidis <a.ioannidis@cern.ch>
2023-09-26 23:55:00 +0200

    models: fix record file indices

--- invenio_records_resources/records/models.py
+++ invenio_records_resources/records/models.py
@@ -34,6 +34,7 @@ class FileRecordModelMixin:
             UUIDType,
             db.ForeignKey(cls.__record_model_cls__.id, ondelete="RESTRICT"),
             nullable=False,
+            index=True,
         )
 
     @declared_attr
@@ -59,9 +60,10 @@ class FileRecordModelMixin:
     def __table_args__(cls):
         """Table args."""
         return (
+            # To make sure we don't have duplicate keys for record files
             db.Index(
-                f"uidx_{cls.__tablename__}_id_key",
-                "id",
+                f"uidx_{cls.__tablename__}_record_id_key",
+                "record_id",
                 "key",
                 unique=True,
             ),
