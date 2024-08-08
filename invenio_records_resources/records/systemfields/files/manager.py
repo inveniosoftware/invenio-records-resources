b66d9f4f07f5360c65bff6e4279915095fcc886c
0einstein0 <fatimah0zulfiqar@gmail.com>
2024-08-08 16:37:00 +0200

    files: sync file access status

--- invenio_records_resources/records/systemfields/files/manager.py
+++ invenio_records_resources/records/systemfields/files/manager.py
@@ -367,11 +367,14 @@ class FilesManager(MutableMapping):
                 else:
                     self[f_key] = obj_or_key
 
-        # Check for metadata changes
+        # Check for metadata and access changes
         for key, dest_rf in self.entries.items():
             if key in src_files:
                 src_rf = src_files[key]
-                if src_rf.metadata != dest_rf.metadata:
+                if (
+                    src_rf.metadata != dest_rf.metadata
+                    or src_rf.access != dest_rf.access
+                ):
                     obj_or_key = dest_rf.object_version
                     self[key] = obj_or_key, dict(src_rf)
 
