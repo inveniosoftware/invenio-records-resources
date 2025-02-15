b6b19f227cbda0214f3be599157219fa4254a167
Mirek Simek <miroslav.simek@gmail.com>
2025-02-15 10:22:13 +0100

    feat: Pluggable transfers and multipart transfer implementation
    * Implementation of RFC 0072 - Pluggable Transfer Types for Record Files
    
    * Pluggable transfer types in invenio_records_resources/services/files/transfers
    
    * Implementation of multipart transfer in the same place
    
    * Permission generator for per-transfer-type permissions
    
    Co-authored-by: Mirek Simek <miroslav.simek@cesnet.cz>

--- invenio_records_resources/services/files/tasks.py
+++ invenio_records_resources/services/files/tasks.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2022-2024 CERN.
+# Copyright (C) 2025 CESNET.
 #
 # Invenio-Records-Resources is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
 # details.
 
 """Files tasks."""
+
+import traceback
 
 import requests
 from celery import shared_task
@@ -15,6 +18,7 @@ from invenio_access.permissions import system_identity
 
 from ...proxies import current_service_registry
 from ...services.errors import FileKeyNotFoundError
+from .transfer.constants import LOCAL_TRANSFER_TYPE
 
 
 @shared_task(ignore_result=True)
@@ -22,20 +26,52 @@ def fetch_file(service_id, record_id, file_key):
     """Fetch file from external storage."""
     try:
         service = current_service_registry.get(service_id)
-        file_record = service.read_file_metadata(system_identity, record_id, file_key)
-        source_url = file_record._file.file.uri
+        transfer_metadata = service.get_transfer_metadata(
+            system_identity, record_id, file_key
+        )
+        source_url = transfer_metadata["url"]
         # download file
         # verify=True for self signed certificates by default
-        with requests.get(source_url, stream=True, allow_redirects=True) as response:
-            # save file
-            service.set_file_content(
-                system_identity,
-                record_id,
-                file_key,
-                response.raw,  # has read method
+        try:
+            with requests.get(
+                source_url, stream=True, allow_redirects=True
+            ) as response:
+                # save file
+                if response.status_code != 200:
+                    current_app.logger.error(
+                        f"Failed to fetch file from {source_url} with status code {response.status_code}"
+                    )
+                    transfer_metadata["error"] = response.text
+                    service.update_transfer_metadata(
+                        system_identity, record_id, file_key, transfer_metadata
+                    )
+                    return
+                service.set_file_content(
+                    system_identity,
+                    record_id,
+                    file_key,
+                    response.raw,  # has read method
+                )
+                transfer_metadata.pop("url")
+                transfer_metadata["type"] = LOCAL_TRANSFER_TYPE
+                service.update_transfer_metadata(
+                    system_identity, record_id, file_key, transfer_metadata
+                )
+                # commit file
+                service.commit_file(system_identity, record_id, file_key)
+        except Exception as e:
+            current_app.logger.error(e)
+            transfer_metadata["error"] = str(e)
+            service.update_transfer_metadata(
+                system_identity, record_id, file_key, transfer_metadata
             )
-            # commit file
-            service.commit_file(system_identity, record_id, file_key)
+            return
 
     except FileKeyNotFoundError as e:
         current_app.logger.error(e)
+
+    except Exception as e:
+        current_app.logger.error(e)
+        traceback.print_exc()
+        # do not raise an exception as we want the task to be marked as errored
+        raise
