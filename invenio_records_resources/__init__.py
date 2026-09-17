2b724097e5cfac21634f4c6dccbacda77bc68d02
Sam Arbid <samaa2@kth.se>
2026-09-17 11:07:24 +0200

    📦 release: v11.0.4

--- invenio_records_resources/__init__.py
+++ invenio_records_resources/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2020-2026 CERN.
 # SPDX-FileCopyrightText: 2024-2026 Graz University of Technology.
-# SPDX-FileCopyrightText: 2025 KTH Royal Institute of Technology.
+# SPDX-FileCopyrightText: 2025-2026 KTH Royal Institute of Technology.
 # SPDX-FileCopyrightText: 2026 Northwestern University.
 # SPDX-FileCopyrightText: 2026 TU Wien.
 # SPDX-License-Identifier: MIT
@@ -9,6 +9,6 @@
 
 from .ext import InvenioRecordsResources
 
-__version__ = "11.0.3"
+__version__ = "11.0.4"
 
 __all__ = ("__version__", "InvenioRecordsResources")
